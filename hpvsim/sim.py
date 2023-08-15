ac0d966e28b262c052f3444cf176660bbe843bec
jamiecohen <jamie.alex.cohen@gmail.com>
2023-08-15 10:57:30 -0700

    color fix

--- hpvsim/sim.py
+++ hpvsim/sim.py
@@ -528,8 +528,8 @@ class Sim(hpb.BaseSim):
         results['cin3_prevalence_by_genotype'] = init_res('CIN3 prevalence', n_rows=ng, color=stock_colors[3])
         results['cin3_prevalence_by_age'] = init_res('CIN3 prevalence by age', n_rows=na, color=stock_colors[3])
         results['female_hpv_prevalence_by_age'] = init_res('Female HPV prevalence by age', n_rows=na, color=stock_colors[3])
-        results['lsil_prevalence'] = init_res('HPV/CIN1 prevalence', color=stock_colors[3])
-        results['lsil_prevalence_by_age'] = init_res('HPV/CIN1 prevalence by age', n_rows=na, color=stock_colors[3])
+        results['lsil_prevalence'] = init_res('HPV/CIN1 prevalence', color='#5f5cd2')
+        results['lsil_prevalence_by_age'] = init_res('HPV/CIN1 prevalence by age', n_rows=na, color='#5f5cd2')
 
         # Time vector
         results['year'] = self.res_yearvec
